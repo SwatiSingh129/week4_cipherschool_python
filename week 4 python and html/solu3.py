75cb08aeebcc96787cf66a183b534788fc10a164
SwatiSingh129 <119078329+SwatiSingh129@users.noreply.github.com>
2023-01-10 20:53:15 +0530

    Add files via upload

---
+++ week 4 python and html/solu3.py
@@ -0,0 +1,18 @@
+with open ('index.html','r') as webpage:
+    with open('index.html', 'a') as wf:
+        page = webpage.read()
+        link_exist = True
+        while link_exist:
+            pos = page.find('<a href=')
+            if pos == -1:
+            else :
+                first_quote = page.find('\"',pos)
+                second_quote = page.find('\"',first_quote+1)
+                url = page[first_quote+1:second_quote]
+                wf.write(url + '\n')
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Securi
